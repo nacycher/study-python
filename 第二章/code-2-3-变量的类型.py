17d58e608041b676c1064fcb696d1fb3a17c07ca
nacycher <nacycher@163.com>
2025-06-10 09:40:27 +0800

    【add】第二章-变量

---
+++ 第二章/code-2-3-变量的类型.py
@@ -0,0 +1,5 @@
+a = 'afd'
+print(type(a))
+print(isinstance(a, str))
+print(isinstance(a, int))
+print(isinstance(a, float))
---
+++ 第二章/code2-2-变量的修改.py
@@ -0,0 +1,16 @@
+# year = 2023
+# print(year)
+# year = 2024
+# print(year)
+#
+# # 正确执行,不同类型的变量可以相互赋值
+# money = '10元'
+# money = 10
+# print(money)
+
+# 超市打折活动
+price = 10.5
+weight = 7.5
+money = price * weight
+money = money - 5
+print("总共消费了%.2f" % money)
